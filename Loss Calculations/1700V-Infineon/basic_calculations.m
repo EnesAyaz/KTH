0331ece5d6ebef502079bd57feca96553f6e76c9
Enes <enes.ayaz.1996@gmail.com>
2024-10-01 18:10:45 +0200

    updates

---
+++ Loss Calculations/1700V-Infineon/basic_calculations.m
@@ -0,0 +1,235 @@
+number_of_data=1000;
+Eoff_150_Id= linspace(0,700,number_of_data);
+Eon_150_Id= linspace(0,700,number_of_data);
+
+Ecoss_off=0;
+Eoff_150_inter=linspace(Ecoss_off,45,number_of_data); % in mJ
+
+a=0.25;
+Eoff_150_inter =a*(Eoff_150_inter) + (1-a)*(Eoff_150_inter.^2)/45;
+
+Ecoss_on=0;
+Eon_150_inter=linspace(Ecoss_on,70,number_of_data);  % in mJ
+
+b=0.25;
+Eon_150_inter =b*(Eon_150_inter) + (1-b)*(Eon_150_inter.^2)/70;
+
+rds_T1= linspace(25,125,21);
+rds_inter1= linspace(2.1e-3,3.4e-3,21);
+
+rds_T2= linspace(125,175,11);
+rds_inter2= linspace(3.4e-3,4.3e-3,11);
+
+rds_T=[rds_T1 rds_T2(2:end)];
+rds_inter=[rds_inter1 rds_inter2(2:end)];
+
+clear Ecoss_on Ecoss_off number_of_data rds_inter1 rds_inter2 rds_T1 rds_T2
+%%
+fc = 22.5e3; % Carrier frequency
+% die_number=12;
+%  scaling=1;
+%%
+% Scaling with dv/dt and di/dt
+% if scaling ~=0
+% a=Eon_150_inter(1);
+% b= Eoff_150_inter(1);
+% 
+% Eon_150_inter=Eon_150_inter-a;
+% Eoff_150_inter=Eoff_150_inter-b;
+% 
+% Eon_150_inter=Eon_150_inter/scaling;
+% Eoff_150_inter=Eoff_150_inter/scaling;
+% 
+% 
+% Eon_150_inter=Eon_150_inter+a;
+% Eoff_150_inter=Eoff_150_inter+b;
+% end
+%% Scaling die area
+% die_number=4;
+% rds_inter=rds_inter*10;
+% Eon_150_inter= Eon_150_inter-(Eon_150_inter(1))+Eon_150_inter(1)/10;
+% Eoff_150_inter= Eoff_150_inter-(Eoff_150_inter(1))+Eoff_150_inter(1)/10;
+% 
+% rds_inter=rds_inter/die_number;
+% 
+% Eon_150_inter=Eon_150_inter+ Eon_150_inter(1)*die_number;
+% Eoff_150_inter=Eoff_150_inter+ Eoff_150_inter(1)*die_number;
+%%
+figure1 = figure;
+% Create axes
+axes1 = axes('Parent',figure1);
+hold(axes1,'on');
+% Create plot
+plot(Eon_150_Id , Eon_150_inter,'DisplayName','Switch-on','Marker','x','LineWidth',0.1,...
+    'Color',[1 0 0]);
+% Create plot
+plot(Eoff_150_Id , Eoff_150_inter,'DisplayName','Switch.off','Marker','x','LineWidth',0.1,...
+    'Color',[0 0 1]);
+% Create ylabel
+ylabel({'Switching Energies (mJ)'});
+% Create xlabel
+xlabel({'Current (A)'});
+box(axes1,'on');
+hold(axes1,'off');
+% Set the remaining axes properties
+set(axes1,'FontName','Times New Roman','FontSize',14);
+% Create legend
+legend1 = legend(axes1,'show');
+set(legend1,...
+    'Position',[0.236309526683319 0.729166666666667 0.238690473316681 0.101010101010102]);
+%%
+        ma=1.15;   % Modulation index
+        f1 = 500; % Fundamental frequency
+        % fc = 11e3; % Carrier frequency
+        pn = fc/f1; % Pulse number
+        npoints = 10000; % Number of timepoints
+        carrytype='tria'; % carrier type 
+        smp= 'ns';  % reference sampling mode 
+        cmode='tri6'; % reference common-mode injection 
+        theta0=0; % reference phase offset
+        thetac=0; % carrier phase offset
+        start_angle= 0; % reference angle to start with
+        end_angle=2*pi; %reference angle to end with 
+        ma_dc=0; % DC reference 
+        [vp,wt,carr,ref] = mod_2lcarr(ma, pn,  npoints ,carrytype,smp,cmode,theta0,thetac,start_angle,end_angle,ma_dc); 
+        % Numerical waveforms during one cycle
+
+        Ud = 1250; % p2p DC voltage
+        P = 300e3;    % Only 200 kW
+        cosphi =0.9;  % Cos(phi) at inverter terminal
+        uppk = ma*Ud/2; % Peak phase voltage reference;
+        ippk = P*2/3/uppk/cosphi;  % Peak phase current
+        phi= acos(cosphi);       % Load angle
+        ip = ippk*cos(wt-phi);   % sampled phase current over one cycle
+        Vp=(ma*Ud/2)*cos(wt) ;   % sampled phase voltage over one cycle
+
+        sw = (ip>=0&vp>0) | (ip<0&vp<=0);   % =1 when current passes through switch
+        di = ~sw;                               % =1 when current passes through diode
+        Isw = abs(ip.*sw);                     % current through switch
+        Idi = abs(ip.*di);                     % current through diode
+        
+        comm = sw - [sw(end) sw(1:end-1)];   % Nonzero at commutations (NOTE includes natural as well as forced commutations)
+ 
+        s2d=find(comm < -eps);          % Find the instants (samples) when current commutates from switch to diode
+        d2s=find(comm > eps);           % Find the instants (samples) when current commutates from diode to switch
+        is2d = abs(ip(s2d));            % Current at commutations switch to diode
+        id2s = abs(ip(d2s));            % Current at commutations diode to switch
+
+%% Conduction
+Tjx= [];
+Ploss_con_Tj=[];
+
+for Tj=25:5:175
+% Define your arrays
+% Tj= 125; % The value you want to find the nearest point for
+% Find the index of the nearest point in array1
+[~, index] = min(abs(rds_T - Tj));
+rds_Tj= rds_inter(index);
+Ploss_con_time_leg=(ip.^2)*rds_Tj;
+Ploss_con = mean(Ploss_con_time_leg)*3;
+Ploss_con_Tj=[Ploss_con_Tj Ploss_con];
+Tjx=[Tjx Tj];
+end
+ % plot(Tjx,Ploss_con_Tj)
+%
+figure1 = figure;
+% Create axes
+axes1 = axes('Parent',figure1);
+hold(axes1,'on');
+% Create plot
+plot(Tjx,Ploss_con_Tj,"Marker",'x','LineWidth',3,...
+    'Color',[1 0 0]);
+% Create ylabel
+ylabel({'Conduction Loss (W)'});
+% Create xlabel
+xlabel({'Tj (C^o)'});
+box(axes1,'on');
+hold(axes1,'off');
+% Set the remaining axes properties
+set(axes1,'FontName','Times New Roman','FontSize',14);
+% Create legend
+% legend1 = legend(axes1,'show');
+% set(legend1,...
+    % 'Position',[0.236309526683319 0.729166666666667 0.238690473316681 0.101010101010102]);
+%%
+%%
+Eon=[];
+for i= 1:length(id2s)
+[~, index] = min(abs( id2s(i)- Eon_150_Id));
+Eon=[Eon Eon_150_inter(index)];
+end
+%%
+Eoff=[];
+for i= 1:length(is2d)
+[~, index] = min(abs( is2d(i)- Eoff_150_Id));
+Eoff=[Eoff Eoff_150_inter(index)];
+end
+
+% plot(Eon)
+% hold off
+% plot(Eoff)
+%%
+fon=linspace(0,1000\f1,length(Eon));
+foff=linspace(0,1000\f1,length(Eoff));
+figure1 = figure;
+% Create axes
+axes1 = axes('Parent',figure1);
+hold(axes1,'on');
+% Create plot
+stem(fon,Eon,'DisplayName','Eon',"Marker",'x','LineWidth',3,...
+    'Color',[1 0 0]);
+yline(3*sum(Eon)*f1/1000,'DisplayName','P_{sw-on}','LineWidth',3,'Color',[0 0 1]);
+% Create ylabel
+ylabel({'Energy (Joule), Switching  Loss (W)'});
+% Create xlabel
+xlabel({'Time (ms)'});
+box(axes1,'on');
+hold(axes1,'off');
+% Set the remaining axes properties
+set(axes1,'FontName','Times New Roman','FontSize',14);
+legend1 = legend(axes1,'show');
+set(legend1,...
+    'Position',[0.236309526683319 0.729166666666667 0.238690473316681 0.101010101010102]);
+% 
+% xlim([0 2*pi])
+
+%%
+fon=linspace(0,1000\f1,length(Eon));
+foff=linspace(0,1000\f1,length(Eoff));
+figure1 = figure;
+% Create axes
+axes1 = axes('Parent',figure1);
+hold(axes1,'on');
+% Create plot
+stem(fon,Eoff,'DisplayName','Eoff',"Marker",'x','LineWidth',3,...
+    'Color',[1 0 0]);
+yline(3*sum(Eoff)*f1/1000,'DisplayName','P_{sw-off}','LineWidth',3,'Color',[0 0 1]);
+% Create ylabel
+ylabel({'Energy (Joule), Switching  Loss (W)'});
+% Create xlabel
+xlabel({'Time (ms)'});
+box(axes1,'on');
+hold(axes1,'off');
+% Set the remaining axes properties
+set(axes1,'FontName','Times New Roman','FontSize',14);
+legend1 = legend(axes1,'show');
+set(legend1,...
+    'Position',[0.236309526683319 0.729166666666667 0.238690473316681 0.101010101010102]);
+
+
+% close all
+%%
+P_loss_switching= 3*(sum(Eon)+sum(Eoff))*f1/1000;
+
+Pcon=Ploss_con_Tj(21); % Tj 125
+Ptot= P_loss_switching+ Pcon;
+
+eff=300e3/(300e3+Ptot);
+
+[P_loss_switching Pcon Ptot]
+
+%%
+% i=21;
+% Pcon=Ploss_con_Tj(i)
+% rds_T(i)
+
--- Loss Calculations/2000V/DieAreaSwitchdvdt.m
+++ Loss Calculations/2000V/DieAreaSwitchdvdt.m
@@ -1,179 +1,191 @@
-Tj=100;
-Ptot_y=[];
-number_of_parallel_dies_x=1:0.5:40;
-fc_x=5e3:0.5e3:30e3;
+Tj=100; 
+Ptot_y=[]; 
+number_of_parallel_dies_x=1:0.5:40; 
+fc_x=5e3:0.5e3:30e3; 
+ 
+% number_of_parallel_dies_x=8:10; 
+% fc_x=5e3:1e3:30e3; 
+ 
+% di_dt_on= 5e9; % A/s 
+% dv_dt_on= 10e9; % V/s 
+% di_dt_off= 20e9; % A/s 
+% dv_dt_off= 15e9; % V/s 
+ 
+ 
+% di_dt_on= 20e9; % A/s 
+% dv_dt_on= 30e9; % V/s 
+% di_dt_off= 60e9; % A/s 
+% dv_dt_off= 30e9; % V/s 
+ 
+di_dt_on= 30e9; % A/s 
+dv_dt_on= 30e9; % V/s 
+di_dt_off= 30e9; % A/s 
+dv_dt_off= 30e9; % V/s 
+ 
+ 
+di_dt_on= 20e9; % A/s 
+dv_dt_on= 10e9; % V/s 
+di_dt_off= 20e9; % A/s 
+dv_dt_off= 10e9; % V/s 
+ 
+ 
+for number_of_parallel_dies= number_of_parallel_dies_x 
+Ptot_x=[]; 
+for fc = fc_x % Carrier frequency 
+rds=25e-4*(1+(Tj-25)/75); 
+rds=rds*8/number_of_parallel_dies; 
+E_zcs= 2e-3; 
+% E_zcs= 0; 
+E_zcs=E_zcs*number_of_parallel_dies/8; 
+ 
+ma=1.15;   % Modulation index 
+f1 = 500; % Fundamental frequency 
+pn = fc/f1; % Pulse number 
+npoints = 10000; % Number of timepoints 
+carrytype='tria'; % carrier type  
+smp= 'ns';  % reference sampling mode  
+cmode='tri6'; % reference common-mode injection  
+theta0=0; % reference phase offset 
+thetac=0; % carrier phase offset 
+start_angle= 0; % reference angle to start with 
+end_angle=2*pi; %reference angle to end with  
+ma_dc=0; % DC reference  
+[vp,wt,carr,ref] = mod_2lcarr(ma, pn,  npoints ,carrytype,smp,cmode,theta0,thetac,start_angle,end_angle,ma_dc);  
+        % Numerical waveforms during one cycle 
+ 
+Ud = 1250; % p2p DC voltage 
+P = 300e3;    % Only 200 kW 
+cosphi =0.9;  % Cos(phi) at inverter terminal 
+uppk = ma*Ud/2; % Peak phase voltage reference; 
+ippk = P*2/3/uppk/cosphi;  % Peak phase current 
+phi= acos(cosphi);       % Load angle 
+ip = ippk*cos(wt-phi);   % sampled phase current over one cycle 
+Vp=(ma*Ud/2)*cos(wt) ;   % sampled phase voltage over one cycle 
+ 
+sw = (ip>=0&vp>0) | (ip<0&vp<=0);   % =1 when current passes through switch 
+di = ~sw;                               % =1 when current passes through diode 
+Isw = abs(ip.*sw);                     % current through switch 
+Idi = abs(ip.*di);                     % current through diode 
+ 
+comm = sw - [sw(end) sw(1:end-1)];   % Nonzero at commutations (NOTE includes natural as well as forced commutations) 
+ 
+s2d=find(comm < -eps);          % Find the instants (samples) when current commutates from switch to diode 
+d2s=find(comm > eps);           % Find the instants (samples) when current commutates from diode to switch 
+is2d = abs(ip(s2d));            % Current at commutations switch to diode 
+id2s = abs(ip(d2s));            % Current at commutations diode to switch 
+ 
+% Conduction 
+Ploss_con_time_leg=(ip.^2)*rds; 
+Ploss_con_3Leg = mean(Ploss_con_time_leg)*3; 
+ 
+% Switch onn 
+Eon=[]; 
+for i= 1:length(id2s) 
+ 
+IL=id2s(i); 
+Udc=Ud; 
+Eon_1= ((Udc*IL.^2/(2*di_dt_on))+(Udc^2.*IL/(2*dv_dt_on))); 
+ 
+Eon_x=Eon_1+E_zcs; 
+ 
+Eon=[Eon Eon_x]; 
+ 
+end 
+ 
+P_loss_switching_on= 3*(sum(Eon))*f1; 
+ 
+% Switch off 
+Eoff=[]; 
+ 
+for i= 1:length(is2d) 
+ 
+IL=is2d(i); 
+Udc=Ud; 
+Eoff_x= ((Udc*IL.^2/(2*di_dt_off))+(Udc^2.*IL/(2*dv_dt_off))); 
+ 
+Eoff=[Eoff Eoff_x]; 
+end 
+ 
+P_loss_switching_off= 3*(sum(Eoff))*f1; 
+% 
+P_loss_switching= P_loss_switching_on+P_loss_switching_off; 
+Pcon=Ploss_con_3Leg; 
+% 
+Ptot= P_loss_switching+ Pcon; 
+ 
+[ P_loss_switching Pcon Ptot]/1e3 
+ 
+eff=300e3/(300e3+Ptot); 
+ 
+Ptot_x=[Ptot_x Ptot]; 
+ 
+end 
+ 
+Ptot_y=[Ptot_y ; Ptot_x]; 
+ 
+end 
+ 
+[X,Y] = meshgrid(fc_x/1e3,number_of_parallel_dies_x); 
+ 
+%% 
+figure1 = figure(); 
+colormap("jet") 
+ 
+% Create axes 
+axes1 = axes('Parent',figure1); 
+hold(axes1,'on'); 
+ 
+hLines = 500;  
+[~,h] = contour(X,Y,Ptot_y,hLines,'Fill','on',"EdgeAlpha",1,"FaceAlpha",1,'DisplayName','Loss (W)'); 
+ 
+targetLoss = 1500; 
+contour(X, Y, Ptot_y, [targetLoss, targetLoss], 'LineColor', [1 1 1], 'LineWidth', 2,'DisplayName','Loss Limit at 1500 W'); 
+colorbar() 
+ 
+% Create xline 
+% xline(10,'DisplayName','10 kHz','Parent',axes1,'LineStyle','--',... 
+%     'Color',[1 1 1],... 
+%     'LineWidth',2); 
+ 
+% Create xline 
+xline(25,'DisplayName','25 kHz','Parent',axes1,'LineStyle','-.',... 
+    'Color',[1 1 1],... 
+    'LineWidth',2); 
+ 
+% Create ylabel 
+ylabel('Number of Parallel Dies','FontName','Times'); 
+ 
+% Create xlabel 
+xlabel('Switching Frequency (kHz)','FontName','Times'); 
+ 
+axis(axes1,'tight'); 
+hold(axes1,'off'); 
+% Set the remaining axes properties 
+set(axes1,'FontName','Times','FontSize',20); 
+% Create colorbar 
+colorbar(axes1); 
+ 
+% title_x= strcat(  'dv/dt ',  '= ', string(dv_dt_on/1e9),  'V/nS '); 
+% title(title_x,'FontSize',15); 
+ 
+% Create legend 
+legend1 = legend(axes1,'show'); 
+set(legend1,'TextColor',[1 1 1],... 
+    'Position',[0.40059524410892 0.574603179002566 0.39642856232822 0.23214285061473],... 
+    'FontSize',15,... 
+    'FontName','Times New Roman',... 
+    'EdgeColor','none',... 
+    'Color','none'); 
 
-% number_of_parallel_dies_x=8:10;
-% fc_x=5e3:1e3:30e3;
-
-% di_dt_on= 5e9; % A/s
-% dv_dt_on= 10e9; % V/s
-% di_dt_off= 20e9; % A/s
-% dv_dt_off= 15e9; % V/s
 
 
-% di_dt_on= 20e9; % A/s
-% dv_dt_on= 30e9; % V/s
-% di_dt_off= 60e9; % A/s
-% dv_dt_off= 30e9; % V/s
-
-di_dt_on= 30e9; % A/s
-dv_dt_on= 30e9; % V/s
-di_dt_off= 30e9; % A/s
-dv_dt_off= 30e9; % V/s
 
 
-di_dt_on= 20e9; % A/s
-dv_dt_on= 10e9; % V/s
-di_dt_off= 20e9; % A/s
-dv_dt_off= 10e9; % V/s
 
 
-for number_of_parallel_dies= number_of_parallel_dies_x
-Ptot_x=[];
-for fc = fc_x % Carrier frequency
-rds=25e-4*(1+(Tj-25)/75);
-rds=rds*8/number_of_parallel_dies;
-E_zcs= 2e-3;
-% E_zcs= 0;
-E_zcs=E_zcs*number_of_parallel_dies/8;
 
-ma=1.15;   % Modulation index
-f1 = 500; % Fundamental frequency
-pn = fc/f1; % Pulse number
-npoints = 10000; % Number of timepoints
-carrytype='tria'; % carrier type 
-smp= 'ns';  % reference sampling mode 
-cmode='tri6'; % reference common-mode injection 
-theta0=0; % reference phase offset
-thetac=0; % carrier phase offset
-start_angle= 0; % reference angle to start with
-end_angle=2*pi; %reference angle to end with 
-ma_dc=0; % DC reference 
-[vp,wt,carr,ref] = mod_2lcarr(ma, pn,  npoints ,carrytype,smp,cmode,theta0,thetac,start_angle,end_angle,ma_dc); 
-        % Numerical waveforms during one cycle
 
-Ud = 1250; % p2p DC voltage
-P = 300e3;    % Only 200 kW
-cosphi =0.9;  % Cos(phi) at inverter terminal
-uppk = ma*Ud/2; % Peak phase voltage reference;
-ippk = P*2/3/uppk/cosphi;  % Peak phase current
-phi= acos(cosphi);       % Load angle
-ip = ippk*cos(wt-phi);   % sampled phase current over one cycle
-Vp=(ma*Ud/2)*cos(wt) ;   % sampled phase voltage over one cycle
 
-sw = (ip>=0&vp>0) | (ip<0&vp<=0);   % =1 when current passes through switch
-di = ~sw;                               % =1 when current passes through diode
-Isw = abs(ip.*sw);                     % current through switch
-Idi = abs(ip.*di);                     % current through diode
 
-comm = sw - [sw(end) sw(1:end-1)];   % Nonzero at commutations (NOTE includes natural as well as forced commutations)
 
-s2d=find(comm < -eps);          % Find the instants (samples) when current commutates from switch to diode
-d2s=find(comm > eps);           % Find the instants (samples) when current commutates from diode to switch
-is2d = abs(ip(s2d));            % Current at commutations switch to diode
-id2s = abs(ip(d2s));            % Current at commutations diode to switch
-
-% Conduction
-Ploss_con_time_leg=(ip.^2)*rds;
-Ploss_con_3Leg = mean(Ploss_con_time_leg)*3;
-
-% Switch onn
-Eon=[];
-for i= 1:length(id2s)
-
-IL=id2s(i);
-Udc=Ud;
-Eon_1= ((Udc*IL.^2/(2*di_dt_on))+(Udc^2.*IL/(2*dv_dt_on)));
-
-Eon_x=Eon_1+E_zcs;
-
-Eon=[Eon Eon_x];
-
-end
-
-P_loss_switching_on= 3*(sum(Eon))*f1;
-
-% Switch off
-Eoff=[];
-
-for i= 1:length(is2d)
-
-IL=is2d(i);
-Udc=Ud;
-Eoff_x= ((Udc*IL.^2/(2*di_dt_off))+(Udc^2.*IL/(2*dv_dt_off)));
-
-Eoff=[Eoff Eoff_x];
-end
-
-P_loss_switching_off= 3*(sum(Eoff))*f1;
-%
-P_loss_switching= P_loss_switching_on+P_loss_switching_off;
-Pcon=Ploss_con_3Leg;
-%
-Ptot= P_loss_switching+ Pcon;
-
-[ P_loss_switching Pcon Ptot]/1e3
-
-eff=300e3/(300e3+Ptot);
-
-Ptot_x=[Ptot_x Ptot];
-
-end
-
-Ptot_y=[Ptot_y ; Ptot_x];
-
-end
-
-[X,Y] = meshgrid(fc_x/1e3,number_of_parallel_dies_x);
-
-%%
-figure1 = figure();
-colormap("jet")
-
-% Create axes
-axes1 = axes('Parent',figure1);
-hold(axes1,'on');
-
-hLines = 500; 
-[~,h] = contour(X,Y,Ptot_y,hLines,'Fill','on',"EdgeAlpha",1,"FaceAlpha",1,'DisplayName','Loss (W)');
-
-targetLoss = 1500;
-contour(X, Y, Ptot_y, [targetLoss, targetLoss], 'LineColor', [1 1 1], 'LineWidth', 2,'DisplayName','Loss Limit at 1500 W');
-colorbar()
-
-% Create xline
-% xline(10,'DisplayName','10 kHz','Parent',axes1,'LineStyle','--',...
-%     'Color',[1 1 1],...
-%     'LineWidth',2);
-
-% Create xline
-xline(25,'DisplayName','25 kHz','Parent',axes1,'LineStyle','-.',...
-    'Color',[1 1 1],...
-    'LineWidth',2);
-
-% Create ylabel
-ylabel('Number of Parallel Dies','FontName','Times');
-
-% Create xlabel
-xlabel('Switching Frequency (kHz)','FontName','Times');
-
-axis(axes1,'tight');
-hold(axes1,'off');
-% Set the remaining axes properties
-set(axes1,'FontName','Times','FontSize',20);
-% Create colorbar
-colorbar(axes1);
-
-% title_x= strcat(  'dv/dt ',  '= ', string(dv_dt_on/1e9),  'V/nS ');
-% title(title_x,'FontSize',15);
-
-% Create legend
-legend1 = legend(axes1,'show');
-set(legend1,'TextColor',[1 1 1],...
-    'Position',[0.40059524410892 0.574603179002566 0.39642856232822 0.23214285061473],...
-    'FontSize',15,...
-    'FontName','Times New Roman',...
-    'EdgeColor','none',...
-    'Color','none');
 
