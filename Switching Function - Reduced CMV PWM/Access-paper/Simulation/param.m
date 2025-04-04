bd23283d4c06124f9434f0f355685749a48a43c7
UG\enesa <enes.ayaz.1996@gmail.com>
2025-04-04 18:41:47 +0200

    ACCESS paper, simulations  and imperix code. Inductance and capacitance values will be selected.

---
+++ Switching Function - Reduced CMV PWM/Access-paper/Simulation/param.m
@@ -0,0 +1,21 @@
+%% Time
+Tfinal = 1;
+SampleTime = 1e-6;
+
+%% Source parameters
+Vdc = 50;
+
+%% PWM common parameters
+fsw = 10e3;
+ffund = 10;
+
+%% PWM different parameters
+ModulationIndex1 = 0.9;
+
+%% Load parameters
+Rload1=5;
+Lload1=500e-6;
+
+
+
+
